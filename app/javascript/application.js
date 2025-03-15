3c1b754553e08ec173928239f7d35ef28ee09fb0
Cesar Krampanis <cesarkrampanis@cesar-krampanis.local>
2025-03-14 23:27:41 -0300

    bootstrap 5, css, devise application layout

---
+++ app/javascript/application.js
@@ -0,0 +1,15 @@
+// Entry point for the build script in your package.json
+
+
+import Rails from "@rails/ujs";
+Rails.start();
+
+import "bootstrap"
+import "bootstrap/dist/css/bootstrap.min.css"
+
+import "./custom/sidebar";
+
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "@fortawesome/fontawesome-free/js/all.min.js";
+
+import "./stylesheets/styleadmin.css";
